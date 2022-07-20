ebb537818c63933a43d0f3835c369de3fe89e5db
fatih <fatihatasoypa@gmail.com>
2022-07-20 22:45:52 +0300

    WebElement is added

--- src/test/java/net/upgenix/pages/CalenderPage_Fatih.java
+++ src/test/java/net/upgenix/pages/CalenderPage_Fatih.java
@@ -1,6 +1,8 @@
 package net.upgenix.pages;
 
 import net.upgenix.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class CalenderPage_Fatih {
@@ -9,6 +11,7 @@ public class CalenderPage_Fatih {
         PageFactory.initElements(Driver.getDriver(),this);
     }
 
-
+@FindBy ()
+    public WebElement aa;
 
 }
