f84949123e572b4ce0ce61124cc816345067c207
fatih <fatihatasoypa@gmail.com>
2022-07-20 21:30:45 +0300

    Project added 2

---
+++ src/test/java/net/upgenix/pages/CalenderPage_Fatih.java
@@ -0,0 +1,14 @@
+package net.upgenix.pages;
+
+import net.upgenix.utilities.Driver;
+import org.openqa.selenium.support.PageFactory;
+
+public class CalenderPage_Fatih {
+
+    public CalenderPage_Fatih(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+
+
+}
---
+++ src/test/java/net/upgenix/runners/CukesRunner.java
@@ -0,0 +1,29 @@
+package net.upgenix.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {
+
+                "html:target/cucumber-report.html",
+                "rerun:target/rerun.txt",
+                "me.jvt.cucumber.report.PrettyReports:target/cucumber",
+                "json:target/cucumber.json"
+
+        },
+        features = "src/test/resources/features",
+        glue = "net/upgenix/step_definitions",
+        dryRun = false,
+        tags = ""
+
+
+)
+public class CukesRunner {
+}
+
+
