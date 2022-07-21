097426d2c012264e45f6536bf49951063475f59e
fatih <fatihatasoypa@gmail.com>
2022-07-21 14:40:19 +0300

    Login page with valid credentials steps is inserted in hooks class @Before annotation

---
+++ src/test/java/net/upgenix/step_definitions/test_StepDefinition.java
@@ -0,0 +1,13 @@
+package net.upgenix.step_definitions;
+
+import io.cucumber.java.en.When;
+
+public class test_StepDefinition {
+    @When("TEST")
+    public void test() {
+        System.out.println();
+    }
+
+
+
+}
